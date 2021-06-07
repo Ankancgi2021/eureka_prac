484969a0870de0c265055c0aec14048a86210f56
MyAsgnmnt <ankanghoshcs.1620@gmail.com>
2021-06-07 09:28:47 +0530

    Eureka to git

---
+++ discover-server/src/test/java/com/practice/discoverserver/DiscoverServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.discoverserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
